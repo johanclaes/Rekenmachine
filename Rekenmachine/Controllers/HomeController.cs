ce8389c1163848a039f99a6c790b8e273100e6cd
johanclaes <s0085147@student.thomasmore.be>
2023-11-07 21:08:42 +0100

    Add project files.

---
+++ Rekenmachine/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rekenmachine.Models;
+using System.Diagnostics;
+
+namespace Rekenmachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Bereken()
+        {
+            return View();
+        }
+
+        [HttpPost("Bereken")]
+        public IActionResult Bereken(double num1, double num2, string bewerking)
+        {
+            double resultaat = 0;
+
+            switch (bewerking)
+            {
+                case "optellen":
+                    resultaat = num1 + num2;
+                    break;
+                case "aftrekken":
+                    resultaat = num1 - num2;
+                    break;
+                case "vermenigvuldigen":
+                    resultaat = num1 * num2;
+                    break;
+                case "delen":
+                    if (num2 != 0)
+                        resultaat = num1 / num2;
+                    else
+                        resultaat = double.NaN;
+                    break;
+            }
+
+            ViewData["Resultaat"] = resultaat;
+
+            return View();
+
+        }
+        public IActionResult NaamOmgekeerd()
+        {
+            return View();
+        }
+
+        [HttpPost("NaamOmgekeerd")]
+        public IActionResult NaamOmgekeerd(string naam)
+        {
+            char[] Tekenreeks = naam.ToCharArray();
+            Array.Reverse(Tekenreeks);
+            string omgekeerdeNaam = new(Tekenreeks);
+            ViewData["OmgekeerdeNaam"] = omgekeerdeNaam;
+            return View();
+        }
+
+
+    }
+
+}
---
+++ Rekenmachine_tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rekenmachine.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Rekenmachine_tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTests
+    {
+        [TestCase(2, 3, "optellen", 5)]
+        [TestCase(5, 3, "aftrekken", 2)]
+        [TestCase(4, 5, "vermenigvuldigen", 20)]
+        [TestCase(6, 2, "delen", 3)]
+        public void TestBereken(double num1, double num2, string bewerking, double verwachtResultaat)
+        {
+            // Arrange
+            var controller = new HomeController();
+
+            // Act
+            var resultaat = controller.Bereken(num1, num2, bewerking) as ViewResult;
+
+            // Assert
+            Assert.That(resultaat.ViewData["Resultaat"], Is.EqualTo(verwachtResultaat));
+
+        }
+
+        [Test]
+        public void TestNaamOmgekeerd()
+        {
+            // Arrange
+            var controller = new HomeController();
+            string naam = "Robocop";
+
+            // Act
+            var resultaat = controller.NaamOmgekeerd(naam) as ViewResult;
+
+            // Assert
+            Assert.That(resultaat, Is.Not.Null);
+            Assert.That(resultaat.ViewData["OmgekeerdeNaam"], Is.EqualTo("pocoboR"));
+        }
+
+    }
+    
+}
